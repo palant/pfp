cf93b392ad0283a95a9f1868c5e4ecb71e267d8b
Wladimir Palant <trev@adblockplus.org>
2015-12-27 20:12:17 +0100

    Fixes #1 - Passwords can be added without a website

--- data/panel/passwordList.js
+++ data/panel/passwordList.js
@@ -4,140 +4,147 @@
  * http://mozilla.org/MPL/2.0/.
  */
 
-"use strict";
-
-let hidePasswordMessagesTimeout = null;
-
-onInit(function()
+(function()
 {
-  setCommandHandler("site-edit", editSite);
-  setCommandHandler("lock-passwords", () => self.port.emit("forgetMasterPassword"));
+  "use strict";
 
-  self.port.on("passwordAdded", showPasswords);
-  self.port.on("passwordRemoved", showPasswords);
-  self.port.on("fillInFailed", showPasswordMessage);
-  self.port.on("passwordCopied", showPasswordMessage.bind(null, "password-copied-message"));
-  self.port.on("passwordCopyFailed", showPasswordMessage);
+  let hidePasswordMessagesTimeout = null;
+  let site = null;
 
-  hidePasswordMessages();
-});
-
-onShow(function({site, passwords})
-{
-  let field = $("site");
-  field.setAttribute("value", site || "???");
-  field.setAttribute("readonly", "readonly");
-
-  showPasswords(passwords);
-});
-
-function hidePasswordMessages()
-{
-  if (hidePasswordMessagesTimeout)
-    window.clearTimeout(hidePasswordMessagesTimeout);
-  hidePasswordMessagesTimeout = null;
-
-  for (let id of ["password-copied-message", "no-such-password", "unknown-generation-method", "wrong-site-message", "no-password-fields"])
-    $(id).hidden = true;
-  resize();
-}
-
-function showPasswordMessage(id)
-{
-  $(id).hidden = false;
-  resize();
-
-  hidePasswordMessagesTimeout = window.setTimeout(hidePasswordMessages, 3000);
-}
-
-function editSite()
-{
-  let field = $("site");
-  field.removeAttribute("readonly");
-  field.focus();
-}
-
-function showPasswords(passwords)
-{
-  let list = $("password-list-container");
-  while (list.lastChild && !list.lastChild.id)
-    list.removeChild(list.lastChild);
-
-  let names = Object.keys(passwords);
-  if (names.length)
+  onInit(function()
   {
-    let template = $("password-template").firstElementChild;
-    for (let link of template.querySelectorAll("a"))
-    {
-      if (link.textContent)
-      {
-        link.setAttribute("title", link.textContent);
-        link.textContent = "";
-      }
-    }
+    setCommandHandler("site-edit", editSite);
+    setCommandHandler("lock-passwords", () => self.port.emit("forgetMasterPassword"));
 
-    names.sort();
-    for (let name of names)
-    {
-      let password = passwords[name];
-      let tooltip;
-      if (password.type == "pbkdf2-sha1-generated")
-      {
-        tooltip = messages["password-type-generated"];
+    self.port.on("passwordAdded", showPasswords);
+    self.port.on("passwordRemoved", showPasswords);
+    self.port.on("fillInFailed", showPasswordMessage);
+    self.port.on("passwordCopied", showPasswordMessage.bind(null, "password-copied-message"));
+    self.port.on("passwordCopyFailed", showPasswordMessage);
 
-        tooltip += "\n" + document.querySelector('label[for="password-length"]').textContent;
-        tooltip += " " + password.length;
+    hidePasswordMessages();
+  });
 
-        tooltip += "\n" + document.querySelector('label[for="charset-lower"]').textContent;
-        if (password.lower)
-          tooltip += " " + "abc";
-        if (password.upper)
-          tooltip += " " + "XYZ";
-        if (password.number)
-          tooltip += " " + "789";
-        if (password.symbol)
-          tooltip += " " + "+^;";
-      }
-      else if (password.type == "pbkdf2-sha1-aes256-encrypted")
-        tooltip = messages["password-type-legacy"];
+  onShow(function({site, passwords})
+  {
+    setSite(site);
+    $("site").setAttribute("readonly", "readonly");
+    showPasswords(passwords);
+  });
 
-      let entry = template.cloneNode(true);
-      setCommandHandler(entry.querySelector(".to-document-link"), fillInPassword.bind(null, name));
-      setCommandHandler(entry.querySelector(".to-clipboard-link"), copyToClipboard.bind(null, name));
-      setCommandHandler(entry.querySelector(".password-remove-link"), removePassword.bind(null, name));
+  function setSite(newSite)
+  {
+    site = newSite;
 
-      let nameNode = entry.querySelector(".password-name");
-      nameNode.textContent = name;
-      nameNode.setAttribute("title", tooltip);
-
-      list.appendChild(entry);
-    }
+    $("site").setAttribute("value", site || "???");
+    $("generate-password-link").hidden = $("legacy-password-link").hidden = !site;
   }
 
-  $("no-passwords-message").hidden = names.length;
-
-  resize();
-}
-
-function fillInPassword(name)
-{
-  let site = $("site").value;
-  self.port.emit("fillIn", {site, name});
-}
-
-function copyToClipboard(name)
-{
-  let site = $("site").value;
-  self.port.emit("copyToClipboard", {site, name});
-}
-
-function removePassword(name)
-{
-  let site = $("site").value;
-  let message = messages["remove-password-confirmation"].replace(/\{1\}/g, name).replace(/\{2\}/g, site);
-  confirm(message).then(response =>
+  function hidePasswordMessages()
   {
-    if (response)
-      self.port.emit("removePassword", {site, name});
-  });
-}
+    if (hidePasswordMessagesTimeout)
+      window.clearTimeout(hidePasswordMessagesTimeout);
+    hidePasswordMessagesTimeout = null;
+
+    for (let id of ["password-copied-message", "no-such-password", "unknown-generation-method", "wrong-site-message", "no-password-fields"])
+      $(id).hidden = true;
+    resize();
+  }
+
+  function showPasswordMessage(id)
+  {
+    $(id).hidden = false;
+    resize();
+
+    hidePasswordMessagesTimeout = window.setTimeout(hidePasswordMessages, 3000);
+  }
+
+  function editSite()
+  {
+    let field = $("site");
+    field.removeAttribute("readonly");
+    field.focus();
+  }
+
+  function showPasswords(passwords)
+  {
+    let list = $("password-list-container");
+    while (list.lastChild && !list.lastChild.id)
+      list.removeChild(list.lastChild);
+
+    let names = Object.keys(passwords);
+    if (names.length)
+    {
+      let template = $("password-template").firstElementChild;
+      for (let link of template.querySelectorAll("a"))
+      {
+        if (link.textContent)
+        {
+          link.setAttribute("title", link.textContent);
+          link.textContent = "";
+        }
+      }
+
+      names.sort();
+      for (let name of names)
+      {
+        let password = passwords[name];
+        let tooltip;
+        if (password.type == "pbkdf2-sha1-generated")
+        {
+          tooltip = messages["password-type-generated"];
+
+          tooltip += "\n" + document.querySelector('label[for="password-length"]').textContent;
+          tooltip += " " + password.length;
+
+          tooltip += "\n" + document.querySelector('label[for="charset-lower"]').textContent;
+          if (password.lower)
+            tooltip += " " + "abc";
+          if (password.upper)
+            tooltip += " " + "XYZ";
+          if (password.number)
+            tooltip += " " + "789";
+          if (password.symbol)
+            tooltip += " " + "+^;";
+        }
+        else if (password.type == "pbkdf2-sha1-aes256-encrypted")
+          tooltip = messages["password-type-legacy"];
+
+        let entry = template.cloneNode(true);
+        setCommandHandler(entry.querySelector(".to-document-link"), fillInPassword.bind(null, name));
+        setCommandHandler(entry.querySelector(".to-clipboard-link"), copyToClipboard.bind(null, name));
+        setCommandHandler(entry.querySelector(".password-remove-link"), removePassword.bind(null, name));
+
+        let nameNode = entry.querySelector(".password-name");
+        nameNode.textContent = name;
+        nameNode.setAttribute("title", tooltip);
+
+        list.appendChild(entry);
+      }
+    }
+
+    $("no-passwords-message").hidden = names.length;
+
+    resize();
+  }
+
+  function fillInPassword(name)
+  {
+    self.port.emit("fillIn", {site, name});
+  }
+
+  function copyToClipboard(name)
+  {
+    self.port.emit("copyToClipboard", {site, name});
+  }
+
+  function removePassword(name)
+  {
+    let message = messages["remove-password-confirmation"].replace(/\{1\}/g, name).replace(/\{2\}/g, site);
+    confirm(message).then(response =>
+    {
+      if (response)
+        self.port.emit("removePassword", {site, name});
+    });
+  }
+})();
