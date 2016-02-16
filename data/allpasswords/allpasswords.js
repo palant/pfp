98d54b63f20ab306919b34c1c62bbcabb437b673
Wladimir Palant <trev@adblockplus.org>
2016-02-16 21:31:03 +0100

    Issue #5 - Allow removing passwords and copying to clipboard on the passwords overview page

--- data/allpasswords/allpasswords.js
+++ data/allpasswords/allpasswords.js
@@ -11,6 +11,30 @@ function $(id)
   return document.getElementById(id);
 }
 
+function setCommandHandler(element, handler)
+{
+  if (typeof element == "string")
+    element = $(element);
+  let wrapper = (event) =>
+  {
+    event.preventDefault();
+    handler.call(element, event);
+  };
+  element.addEventListener("click", wrapper);
+}
+
+function copyToClipboard(site, name, id)
+{
+  self.port.emit("copyToClipboard", {site, name, id});
+}
+
+function removePassword(site, name, id)
+{
+  let message = $("remove-password-confirmation").textContent.replace(/\{1\}/g, name).replace(/\{2\}/g, site);
+  if (confirm(message))
+    self.port.emit("removePassword", {site, name, id});
+}
+
 self.port.on("init", function(sites)
 {
   let siteTemplate = $("site-template").firstElementChild;
@@ -27,6 +51,7 @@ self.port.on("init", function(sites)
   let siteNames = Object.keys(sites);
   siteNames.sort();
 
+  let counter = 0;
   let container = $("list");
   for (let site of siteNames)
   {
@@ -46,7 +71,11 @@ self.port.on("init", function(sites)
     {
       let passwordData = passwords[name];
       let passwordInfo = passwordTemplate.cloneNode(true);
+      passwordInfo.id = "password" + ++counter;
       passwordInfo.querySelector(".password-name").textContent = name;
+
+      setCommandHandler(passwordInfo.querySelector(".to-clipboard-link"), copyToClipboard.bind(null, site, name, passwordInfo.id));
+      setCommandHandler(passwordInfo.querySelector(".password-remove-link"), removePassword.bind(null, site, name, passwordInfo.id));
 
       if (passwordData.type == "pbkdf2-sha1-generated")
       {
@@ -72,5 +101,37 @@ self.port.on("init", function(sites)
     }
 
     container.appendChild(siteInfo);
+  }
+});
+
+self.port.on("passwordError", reason => {
+  let message = $(reason);
+  if (message && message.parentNode.id == "messages")
+    message = message.textContent;
+  else
+    message = reason;
+  alert(message);
+});
+
+self.port.on("passwordRemoved", id => {
+  let passwordInfo = $(id);
+  if (passwordInfo)
+  {
+    let siteInfo = passwordInfo.parentNode;
+    siteInfo.removeChild(passwordInfo);
+    if (!siteInfo.querySelector(".password-container"))
+      siteInfo.parentNode.removeChild(siteInfo);
+  }
+});
+
+self.port.on("passwordCopied", id => {
+  let passwordInfo = $(id);
+  if (passwordInfo)
+  {
+    let message = passwordInfo.querySelector(".password-copied-message");
+    message.hidden = false;
+    setTimeout(() => {
+      message.hidden = true;
+    }, 3000);
   }
 });
