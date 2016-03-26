fc6799c88ba18e91a8584563bce91102457bbbf2
Wladimir Palant <trev@adblockplus.org>
2016-03-27 00:34:04 +0100

    Fixes #10 - Make sure that password fill-in considers frames but adjust error message to indicate its limitations

--- data/fillIn.js
+++ data/fillIn.js
@@ -6,21 +6,44 @@
 
 "use strict";
 
+function fillIn(wnd)
+{
+  let fields = wnd.document.querySelectorAll("input[type=password]");
+  let result = false;
+  fields = Array.filter(fields, element => element.offsetHeight && element.offsetWidth);
+  if (fields.length > 0)
+  {
+    result = true;
+    for (let field of fields)
+    {
+      field.value = password;
+      field.dispatchEvent(new Event("change", {bubbles: true}));
+    }
+    fields[0].focus();
+  }
+
+  for (let i = 0; i < wnd.frames.length; i++)
+  {
+    try
+    {
+      wnd.frames[i].document;
+    }
+    catch (e)
+    {
+      // Forbidden by same-origin policy, ignore
+      continue;
+    }
+    result = result || fillIn(wnd.frames[i]);
+  }
+
+  return result;
+}
+
 let {host, password} = self.options;
-if (location.hostname != host)
+if (window.location.hostname != host)
   throw new Error("Password is meant for a different website, not filling in.");
 
-let fields = document.querySelectorAll("input[type=password]");
-fields = Array.filter(fields, element => element.offsetHeight && element.offsetWidth);
-if (fields.length > 0)
-{
-  for (let field of fields)
-  {
-    field.value = password;
-    field.dispatchEvent(new Event("change", {bubbles: true}));
-  }
-  fields[0].focus();
+if (fillIn(window))
   self.port.emit("success");
-}
 else
   self.port.emit("failure");
