adaf324a4da49643cba2198a19963c05a45efa56
Wladimir Palant <trev@adblockplus.org>
2016-04-25 11:36:16 +0200

    Don't generate temporary files inside the repository, put all build artifacts into the build directory

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,9 @@
 
 let gulp = require("gulp");
 let less = require("gulp-less");
+let rename = require("gulp-rename");
 let del = require("del");
+let path = require("path");
 let spawn = require("child_process").spawn;
 
 function readArg(prefix, defaultValue)
@@ -23,7 +25,9 @@ function jpm(args)
 {
   return new Promise((resolve, reject) =>
   {
-    let ps = spawn("./node_modules/.bin/jpm", args);
+    let builddir = path.resolve(process.cwd(), "build");
+    let jpm = path.resolve(process.cwd(), "node_modules/.bin/jpm");
+    let ps = spawn(jpm, args, {cwd: builddir});
     ps.stdout.pipe(process.stdout);
     ps.stderr.pipe(process.stderr);
     ps.on("close", resolve);
@@ -34,19 +38,66 @@ gulp.task("default", ["xpi"], function()
 {
 });
 
+gulp.task("package.json", function()
+{
+  return gulp.src("package.json")
+             .pipe(gulp.dest("build"));
+});
+
+gulp.task("LICENSE.txt", function()
+{
+  return gulp.src("LICENSE.txt")
+             .pipe(gulp.dest("build"));
+});
+
+gulp.task("icon.png", function()
+{
+  return gulp.src(["data/images/icon48.png"])
+             .pipe(rename("icon.png"))
+             .pipe(gulp.dest("build"));
+});
+
+gulp.task("icon64.png", function()
+{
+  return gulp.src(["data/images/icon64.png"])
+             .pipe(gulp.dest("build"));
+});
+
+gulp.task("data", function()
+{
+  return gulp.src(["data/**/*.js", "data/**/*.html", "data/**/*.png", "data/**/*.svg", "!data/images/icon48.png"])
+             .pipe(gulp.dest("build/data"));
+});
+
 gulp.task("less", function()
 {
   return gulp.src("data/**/*.less")
              .pipe(less())
-             .pipe(gulp.dest('data'));
+             .pipe(gulp.dest("build/data"));
 });
 
-gulp.task("xpi", ["less"], function()
+gulp.task("lib", function()
+{
+  return gulp.src("lib/**/*.js")
+             .pipe(gulp.dest("build/lib"));
+});
+
+gulp.task("locale", function()
+{
+  return gulp.src("locale/**/*.properties")
+             .pipe(gulp.dest("build/locale"));
+});
+
+gulp.task("builddir", ["package.json", "LICENSE.txt", "icon.png", "icon64.png", "data", "less", "lib", "locale"], function()
+{
+});
+
+gulp.task("xpi", ["builddir"], function()
 {
   return jpm(["xpi"]);
 });
 
-gulp.task("post", ["less"], function()
+gulp.task("post", ["builddir"], function()
 {
   let postUrl = readArg("--post-url=", "http://localhost:8888/");
   if (/^\d+$/.test(postUrl))
@@ -64,5 +115,5 @@ gulp.task("watch", ["post"], function()
 
 gulp.task("clean", function()
 {
-  return del("data/**/*.css");
+  return del("build");
 });
