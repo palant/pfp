a7dc60d8857f8428cba3c25dcdf8dbb9705c4cfd
Dave Barker <kzar@kzar.co.uk>
2017-04-02 16:59:16 +0800

    Issue 26 - Allow Lastpass CSV files to be imported

---
+++ test/lastpassImport.js
@@ -0,0 +1,17 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+"use strict";
+
+exports.testParseCSV = function(test)
+{
+  // FIXME - Write these tests...
+};
+
+exports.testParseLastpassCSV = function(test)
+{
+  // FIXME - Write these tests...
+};
