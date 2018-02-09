174813ba07b365ae19cbf2d956519f08ac3eaf29
Wladimir Palant <fqcgithub@palant.de>
2018-02-09 20:59:02 +0100

    Removed Edge printing work-around, no longer necessary with Edge 41

--- data/platform.js
+++ data/platform.js
@@ -20,29 +20,3 @@ window.addEventListener("DOMContentLoaded", function()
     element.textContent = browser.i18n.getMessage(id);
   }
 });
-
-// Work-around for https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/9550897/,
-// inline all stylesheets.
-
-if (window.navigator.userAgent.indexOf(" Edge/") >= 0)
-{
-  window.addEventListener("DOMContentLoaded", () =>
-  {
-    let stylesheets = document.querySelectorAll("link[rel='stylesheet']");
-    for (let i = 0; i < stylesheets.length; i++)
-    {
-      let stylesheet = stylesheets[i];
-      let request = new XMLHttpRequest();
-      request.open("GET", stylesheet.href);
-      request.responseType = "text";
-      request.addEventListener("load", () =>
-      {
-        let element = document.createElement("style");
-        element.setAttribute("media", "print");
-        element.textContent = request.response;
-        stylesheet.parentNode.insertBefore(element, stylesheet.nextSibling);
-      });
-      request.send(null);
-    }
-  });
-}
